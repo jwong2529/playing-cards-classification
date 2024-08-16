fd3e6d575e24421f687e8671cc8941801d678e76
jwong2529 <janice.wong2529@gmail.com>
2024-08-15 22:04:59 -0700

    construct model in graph mode

--- server/index.py
+++ server/index.py
@@ -1,58 +1,61 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-from model_manager import ModelManager
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import model_from_json
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from PIL import Image
+from io import BytesIO
+
+tf.compat.v1.disable_eager_execution()
 
 app = Flask(__name__)
 CORS(app)
 
-# print(tf.__version__)
+print(tf.__version__)
 
 print("Starting server...")
 
-# get the singleton instance of ModelManager
-model_manager = ModelManager.get_instance()
+# load the model architecture and weights
+print("Loading model...")
+with open('playing-cards-model.json', 'r') as json_file:
+    model_json = json_file.read()
 
-# # load the model architecture and weights
-# print("Loading model...")
-# with open('playing-cards-model.json', 'r') as json_file:
-#     model_json = json_file.read()
+model = model_from_json(model_json)
+model.load_weights('playing-cards-model_weights.h5')
 
-# model = model_from_json(model_json)
-# model.load_weights('playing-cards-model_weights.h5')
-
-# # # save the tensorflow graph
-# # tf.compat.v1.disable_eager_execution()
-# # graph = tf.compat.v1.get_default_graph()
-# # model._make_predict_function()
-# global graph
+# # save the tensorflow graph
+# tf.compat.v1.disable_eager_execution()
 # graph = tf.compat.v1.get_default_graph()
+global graph
+graph = tf.compat.v1.get_default_graph()
 
-# IMG_SIZE = (128, 128)  
+IMG_SIZE = (128, 128)  
 
-# class_labels = [
-#     "ace of clubs", "ace of diamonds", "ace of hearts", "ace of spades",
-#     "eight of clubs", "eight of diamonds", "eight of hearts", "eight of spades",
-#     "five of clubs", "five of diamonds", "five of hearts", "five of spades",
-#     "four of clubs", "four of diamonds", "four of hearts", "four of spades",
-#     "jack of clubs", "jack of diamonds", "jack of hearts", "jack of spades",
-#     "joker", "king of clubs", "king of diamonds", "king of hearts",
-#     "king of spades", "nine of clubs", "nine of diamonds", "nine of hearts",
-#     "nine of spades", "queen of clubs", "queen of diamonds", "queen of hearts",
-#     "queen of spades", "seven of clubs", "seven of diamonds", "seven of hearts",
-#     "seven of spades", "six of clubs", "six of diamonds", "six of hearts",
-#     "six of spades", "ten of clubs", "ten of diamonds", "ten of hearts",
-#     "ten of spades", "three of clubs", "three of diamonds", "three of hearts",
-#     "three of spades", "two of clubs", "two of diamonds", "two of hearts",
-#     "two of spades"
-# ]
+class_labels = [
+    "ace of clubs", "ace of diamonds", "ace of hearts", "ace of spades",
+    "eight of clubs", "eight of diamonds", "eight of hearts", "eight of spades",
+    "five of clubs", "five of diamonds", "five of hearts", "five of spades",
+    "four of clubs", "four of diamonds", "four of hearts", "four of spades",
+    "jack of clubs", "jack of diamonds", "jack of hearts", "jack of spades",
+    "joker", "king of clubs", "king of diamonds", "king of hearts",
+    "king of spades", "nine of clubs", "nine of diamonds", "nine of hearts",
+    "nine of spades", "queen of clubs", "queen of diamonds", "queen of hearts",
+    "queen of spades", "seven of clubs", "seven of diamonds", "seven of hearts",
+    "seven of spades", "six of clubs", "six of diamonds", "six of hearts",
+    "six of spades", "ten of clubs", "ten of diamonds", "ten of hearts",
+    "ten of spades", "three of clubs", "three of diamonds", "three of hearts",
+    "three of spades", "two of clubs", "two of diamonds", "two of hearts",
+    "two of spades"
+]
 
-# def preprocess_image(image_data):
-#     """Preprocess the image to the required size and format for the model."""
-#     image = Image.open(BytesIO(image_data)).convert('RGB')
-#     image = image.resize(IMG_SIZE)
-#     image_array = np.array(image) / 255.0  # normalize pixel values to [0, 1]
-#     image_array = np.expand_dims(image_array, axis=0)  # add batch dimension
-#     return image_array
+def preprocess_image(image_data):
+    """Preprocess the image to the required size and format for the model."""
+    image = Image.open(BytesIO(image_data)).convert('RGB')
+    image = image.resize(IMG_SIZE)
+    image_array = np.array(image) / 255.0  # normalize pixel values to [0, 1]
+    image_array = np.expand_dims(image_array, axis=0)  # add batch dimension
+    return image_array
 
 @app.route("/")
 def connect():
@@ -72,32 +75,26 @@ def predict():
         return jsonify({'error': 'No file selected'}), 400
     
     # preprocess the image
-    # processed_image = preprocess_image(file.read())
+    processed_image = preprocess_image(file.read())
     
-    try :
-        result = model_manager.predict(file.read())
-        print(f"Prediction: {result['prediction']}, Confidence: {result['confidence']}")
-        return jsonify(result)
-    except Exception as e:
-        print(f"Error during prediction: {e}")
-        return jsonify({'error': 'Internal Server Error'}), 500
+    # use saved graph for inference
+    with graph.as_default():
+        # Predict
+        print("Making prediction...")
+        predictions = model.predict(processed_image)
+        predicted_class = np.argmax(predictions, axis=1)
+        confidence = np.max(predictions)
 
-    # # Predict
-    # print("Making prediction...")
-    # predictions = model.predict(processed_image)
-    # predicted_class = np.argmax(predictions, axis=1)
-    # confidence = np.max(predictions)
-
-    # # display class if model has at least 40% confidence in its prediction
-    # confidence_threshold = 0.40
-    # if confidence < confidence_threshold:
-    #     predicted_label = "no prediction"
-    # else:
-    #     predicted_label = class_labels[predicted_class[0]]  # map index to label
-    
-    # print(f"Prediction: {predicted_label}, Confidence: {float(confidence)}")
-    
-    # return jsonify({'prediction': predicted_label, 'confidence': float(confidence)})
+        # display class if model has at least 40% confidence in its prediction
+        confidence_threshold = 0.40
+        if confidence < confidence_threshold:
+            predicted_label = "no prediction"
+        else:
+            predicted_label = class_labels[predicted_class[0]]  # map index to label
+        
+        print(f"Prediction: {predicted_label}, Confidence: {float(confidence)}")
+        
+        return jsonify({'prediction': predicted_label, 'confidence': float(confidence)})
 
 if __name__ == '__main__':
     import os
