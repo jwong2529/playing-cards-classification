a389c72c1d215f062b1bca65313e92d77edef7d2
jwong2529 <janice.wong2529@gmail.com>
2024-08-16 02:29:19 -0700

    adjust ui depending on environment

--- client/src/PlayingCardsClassifier.js
+++ client/src/PlayingCardsClassifier.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react';
 import axios from 'axios';
 import {Camera} from "react-camera-pro";
+import {BrowserView, MobileView} from 'react-device-detect';
 import './PlayingCardsClassifier.css'
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
@@ -47,48 +48,72 @@ const PhotoUploadOrCapture = ({ onImageSubmit, onOpenCamera }) => {
 
   return (
     <div style={{ textAlign: 'center' }}>
-      <Typography variant="subtitle2" gutterBottom>
-        Capture or Upload an Image
-      </Typography>
+      <BrowserView>
+        <Typography variant="subtitle2" gutterBottom>
+            Capture or Upload an Image
+        </Typography>
+        <Stack direction="row" spacing={2}>
+          <Button onClick={handleOpenCamera} variant="outlined">Open Camera to Capture Image</Button>
+          <Button onClick={() => document.getElementById('file-upload').click()} variant="outlined">
+            Upload Photo
+          </Button>
+          <label htmlFor="file-upload" className="custom-file-upload">
+            <Typography variant="overline" display="block" gutterBottom>
+            {fileInputLabel}
+            </Typography>
+          </label>
+        </Stack>
 
-      <Stack direction="row" spacing={2}>
-        <Button onClick={handleOpenCamera} variant="outlined">Open Camera to Capture Image</Button>
-        <Button onClick={() => document.getElementById('file-upload').click()} variant="outlined">
-          Upload Photo
-        </Button>
-        <label htmlFor="file-upload" className="custom-file-upload">
-          <Typography variant="overline" display="block" gutterBottom>
-          {fileInputLabel}
-          </Typography>
-        </label>
-      </Stack>
-
-      <div style={{ marginTop: '10px', marginBottom: '10px' }}>
-        <input
-          id="file-upload"
-          type="file"
-          accept="image/*"
-          onChange={handleFileUpload}
-          style={{ display: 'none' }}
-        />
-      </div>
-
-      {isCameraOpen && (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <div style={{ width: '350px', height: '210px', position: 'relative', marginBottom: '10px'}}>
-            <Camera
-              ref={cameraRef}
-              aspectRatio={5 / 3}  // Adjust aspect ratio if needed
-              facingMode='environment'
-              style={{ width: '100%', height: '100%'}}
-            />
-          </div>
-          <Stack direction="row" spacing={2}>
-            <Button onClick={capture} variant="outlined" color="success">Capture photo</Button>
-            <Button onClick={handleCancelCamera} variant="outlined" color="error">Cancel</Button>
-          </Stack>
+        <div style={{ marginTop: '10px', marginBottom: '10px' }}>
+          <input
+            id="file-upload"
+            type="file"
+            accept="image/*"
+            onChange={handleFileUpload}
+            style={{ display: 'none' }}
+          />
         </div>
-      )}
+
+        {isCameraOpen && (
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <div style={{ width: '350px', height: '210px', position: 'relative', marginBottom: '10px'}}>
+              <Camera
+                ref={cameraRef}
+                aspectRatio={5 / 3}  // Adjust aspect ratio if needed
+                facingMode='environment'
+                style={{ width: '100%', height: '100%'}}
+              />
+            </div>
+            <Stack direction="row" spacing={2}>
+              <Button onClick={capture} variant="outlined" color="success">Capture photo</Button>
+              <Button onClick={handleCancelCamera} variant="outlined" color="error">Cancel</Button>
+            </Stack>
+          </div>
+        )}
+      </BrowserView>
+
+      <MobileView>
+        <Stack direction="row" spacing={2}>
+          <Button onClick={() => document.getElementById('file-upload').click()} variant="outlined">
+            Take Or Upload Photo
+          </Button>
+          <label htmlFor="file-upload" className="custom-file-upload">
+            <Typography variant="overline" display="block" gutterBottom>
+            {fileInputLabel}
+            </Typography>
+          </label>
+        </Stack>
+
+        <div style={{ marginTop: '10px', marginBottom: '10px' }}>
+          <input
+            id="file-upload"
+            type="file"
+            accept="image/*"
+            onChange={handleFileUpload}
+            style={{ display: 'none' }}
+          />
+        </div>
+      </MobileView>
 
       {selectedImage && (
         <div style={{ marginTop: '20px' }}>
@@ -131,7 +156,7 @@ const PlayingCardsClassifier = () => {
     formData.append('file', file);
 
   try {
-    const response = await axios.post('https://playing-cards-classifier.onrender.com/predict', formData, {
+    const response = await axios.post('http://localhost:10000/predict', formData, {
       headers: {
         'Content-Type': 'multipart/form-data',
       },
